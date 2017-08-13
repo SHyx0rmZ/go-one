52e5eb570310d3c554c2d198e574a9bb84b99c8b
Patrick Pokatilo <mail@shyxormz.net>
2017-08-13 18:22:03 +0200

    Add tests for ClusterPool

--- api/cluster_pool_test.go
+++ api/cluster_pool_test.go
@@ -1,1 +1,25 @@
-package api
+package api_test
+
+import (
+	"github.com/SHyx0rmZ/go-one/api"
+	"github.com/SHyx0rmZ/go-xmlrpc"
+	"github.com/SHyx0rmZ/go-xmlrpc/mock"
+	"reflect"
+	"testing"
+)
+
+func TestClusterPool_Info(t *testing.T) {
+	c := client(t, "one.clusterpool.info", []arg{
+		{reflect.String, "test-session"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithString("test-info"),
+	})
+
+	i := api.ClusterPool{Client: c}
+
+	v, err := i.Info("test-session")
+	if err != nil || v != "test-info" {
+		t.Errorf("Info() == (%q, %q), want (%q, %q)", v, err, "test-info", nil)
+	}
+}
