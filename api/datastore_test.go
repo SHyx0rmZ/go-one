deb3d001b564ab2210f2fc2f39ce4b0d97ac205f
Patrick Pokatilo <mail@shyxormz.net>
2017-08-14 05:05:47 +0200

    Actually add tests for Datastore

--- api/datastore_test.go
+++ api/datastore_test.go
@@ -1,1 +1,153 @@
-package api
+package api_test
+
+import (
+	"github.com/SHyx0rmZ/go-one/api"
+	"github.com/SHyx0rmZ/go-xmlrpc"
+	"github.com/SHyx0rmZ/go-xmlrpc/mock"
+	"reflect"
+	"testing"
+)
+
+func TestDatastore_Allocate(t *testing.T) {
+	c := client(t, "one.datastore.allocate", []arg{
+		{reflect.String, "test-session"},
+		{reflect.String, "test-template"},
+		{reflect.Int, 42},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(21),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Allocate("test-session", "test-template", 42)
+	if err != nil || v != 21 {
+		t.Errorf("Allocate() == (%v, %q), want (%v, %q)", v, err, 21, nil)
+	}
+}
+
+func TestDatastore_Chmod(t *testing.T) {
+	c := client(t, "one.datastore.chmod", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.Int, 1},
+		{reflect.Int, 1},
+		{reflect.Int, 1},
+		{reflect.Int, 0},
+		{reflect.Int, 0},
+		{reflect.Int, 0},
+		{reflect.Int, -1},
+		{reflect.Int, -1},
+		{reflect.Int, -1},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Chmod("test-session", 42, 1, 1, 1, 0, 0, 0, -1, -1, -1)
+	if err != nil || v != 42 {
+		t.Errorf("Chmod() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
+
+func TestDatastore_Chown(t *testing.T) {
+	c := client(t, "one.datastore.chown", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.Int, -1},
+		{reflect.Int, 21},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Chown("test-session", 42, -1, 21)
+	if err != nil || v != 42 {
+		t.Errorf("Chown() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
+
+func TestDatastore_Delete(t *testing.T) {
+	c := client(t, "one.datastore.delete", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Delete("test-session", 42)
+	if err != nil || v != 42 {
+		t.Errorf("Delete() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
+
+func TestDatastore_Enable(t *testing.T) {
+	c := client(t, "one.datastore.enable", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.Bool, true},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Enable("test-session", 42, true)
+	if err != nil || v != 42 {
+		t.Errorf("Enable() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
+
+func TestDatastore_Info(t *testing.T) {
+	c := client(t, "one.datastore.info", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithString("test-info"),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Info("test-session", 42)
+	if err != nil || v != "test-info" {
+		t.Errorf("Info() == (%q, %q), want (%q, %q)", v, err, "test-info", nil)
+	}
+}
+
+func TestDatastore_Rename(t *testing.T) {
+	c := client(t, "one.datastore.rename", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-name"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Rename("test-session", 42, "test-name")
+	if err != nil || v != 42 {
+		t.Errorf("Rename() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
+
+func TestDatastore_Update(t *testing.T) {
+	c := client(t, "one.datastore.update", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-template"},
+		{reflect.Int, api.UpdateTypeMerge},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Datastore{Client: c}
+	v, err := i.Update("test-session", 42, "test-template", api.UpdateTypeMerge)
+	if err != nil || v != 42 {
+		t.Errorf("Update() == (%v, %q), want (%v, %q)", v, err, 42, nil)
+	}
+}
