382f258167f5108ba2d552e00a61df568bb35059
Patrick Pokatilo <mail@shyxormz.net>
2017-08-14 05:30:55 +0200

    Add tests for Document

---
+++ api/document_test.go
@@ -0,0 +1,188 @@
+package api_test
+
+import (
+	"github.com/SHyx0rmZ/go-one/api"
+	"github.com/SHyx0rmZ/go-xmlrpc"
+	"github.com/SHyx0rmZ/go-xmlrpc/mock"
+	"reflect"
+	"testing"
+)
+
+func TestDocument_Allocate(t *testing.T) {
+	c := client(t, "one.document.allocate", []arg{
+		{reflect.String, "test-session"},
+		{reflect.String, "test-template"},
+		{reflect.Int, 21},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Allocate("test-session", "test-template", 21)
+	if err != nil || v != 42 {
+		t.Errorf("Allocate() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Chmod(t *testing.T) {
+	c := client(t, "one.document.chmod", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.Int, 1},
+		{reflect.Int, 1},
+		{reflect.Int, 1},
+		{reflect.Int, 0},
+		{reflect.Int, 0},
+		{reflect.Int, 0},
+		{reflect.Int, -1},
+		{reflect.Int, -1},
+		{reflect.Int, -1},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Chmod("test-session", 42, 1, 1, 1, 0, 0, 0, -1, -1, -1)
+	if err != nil || v != 42 {
+		t.Errorf("Chmod() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Chown(t *testing.T) {
+	c := client(t, "one.document.chown", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.Int, -1},
+		{reflect.Int, 21},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Chown("test-session", 42, -1, 21)
+	if err != nil || v != 42 {
+		t.Errorf("Chown() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Clone(t *testing.T) {
+	c := client(t, "one.document.clone", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-name"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(21),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Clone("test-session", 42, "test-name")
+	if err != nil || v != 21 {
+		t.Errorf("Clone() == (%v, %v), want (%v, %v)", v, err, 21, nil)
+	}
+}
+
+func TestDocument_Delete(t *testing.T) {
+	c := client(t, "one.document.delete", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Delete("test-session", 42)
+	if err != nil || v != 42 {
+		t.Errorf("Delete() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Info(t *testing.T) {
+	c := client(t, "one.document.info", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithString("test-info"),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Info("test-session", 42)
+	if err != nil || v != "test-info" {
+		t.Errorf("Info() == (%q, %v), want (%q, %v)", v, err, "test-info", nil)
+	}
+}
+
+func TestDocument_Lock(t *testing.T) {
+	c := client(t, "one.document.lock", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-application"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithBool(true),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Lock("test-session", 42, "test-application")
+	if err != nil || v != true {
+		t.Errorf("Lock() == (%v, %v), want (%v, %v)", v, err, true, nil)
+	}
+}
+
+func TestDocument_Rename(t *testing.T) {
+	c := client(t, "one.document.rename", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-name"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Rename("test-session", 42, "test-name")
+	if err != nil || v != 42 {
+		t.Errorf("Rename() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Unlock(t *testing.T) {
+	c := client(t, "one.document.unlock", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-application"},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Unlock("test-session", 42, "test-application")
+	if err != nil || v != 42 {
+		t.Errorf("Unlock() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+}
+
+func TestDocument_Update(t *testing.T) {
+	c := client(t, "one.document.update", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, 42},
+		{reflect.String, "test-template"},
+		{reflect.Int, api.UpdateTypeMerge},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithInt(42),
+	})
+
+	i := api.Document{Client: c}
+	v, err := i.Update("test-session", 42, "test-template", api.UpdateTypeMerge)
+	if err != nil || v != 42 {
+		t.Errorf("Update() == (%v, %v), want (%v, %v)", v, err, 42, nil)
+	}
+
+}
