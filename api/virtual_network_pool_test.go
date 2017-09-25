8b141b99b904befb7614ade44ab67c06c44505fe
Patrick Pokatilo <mail@shyxormz.net>
2017-09-25 13:47:39 +0200

    Add tests for VirtualNetworkPool

---
+++ api/virtual_network_pool_test.go
@@ -0,0 +1,27 @@
+package api_test
+
+import (
+	"github.com/SHyx0rmZ/go-one/api"
+	"github.com/SHyx0rmZ/go-xmlrpc"
+	"github.com/SHyx0rmZ/go-xmlrpc/mock"
+	"reflect"
+	"testing"
+)
+
+func TestVirtualNetworkPool_Info(t *testing.T) {
+	c := client(t, "one.vnpool.info", []arg{
+		{reflect.String, "test-session"},
+		{reflect.Int, api.FilterAll},
+		{reflect.Int, -1},
+		{reflect.Int, -1},
+	}, []xmlrpc.Value{
+		mock.NewValue().WithBool(true),
+		mock.NewValue().WithString("test-info"),
+	})
+
+	i := api.VirtualNetworkPool{Client: c}
+	v, err := i.Info("test-session", api.FilterAll, -1, -1)
+	if err != nil || v != "test-info" {
+		t.Errorf("Info() == (%q, %q), want (%q, %v)", v, err, "test-info", nil)
+	}
+}
